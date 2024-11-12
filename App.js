2ab641020d855e4a30f29aaac22ec85a0c1f4a51
Lihuayang05 <23013744@myrp.edu.sg>
2024-11-12 14:00:37 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ScrollView } from 'react-native';
+import Boat from './img/Boat';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const AllBoats = () => {
+    return (
+        <ScrollView>
+            <Boat
+                name="Sea Ray 500 Sundancer"
+                description="Contoured lines and dramatic styling reveal a refined and powerful presence."
+                iconName="ship"
+                picture={require('./img/sea_ray.jpg')}
+            />
+            <Boat
+                name="Four Winns Horizon 180"
+                description="A sporty look and refined details truly set the Horizon 180 above all others."
+                iconName="anchor"
+                picture={require('./img/four_winns.jpg')}
+            />
+            <Boat
+                name="Flipper 640 ST"
+                description="A modern take on the classic, traditional hardtop and perfect for a family picnic."
+                iconName="water"
+                picture={require('./img/flipper.jpg')}
+            />
+            <Boat
+                name="Princess V48"
+                description="There is the option for an open design with a full-length cockpit and sunroof."
+                iconName="yacht"
+                picture={require('./img/princess.jpg')}
+            />
+            <Boat
+                name="Bayliner 175 Bowrider"
+                description="Its outboard power gives you increased cockpit space and fuel-efficient performance."
+                iconName="sailboat"
+                picture={require('./img/bayliner.jpg')}
+            />
+            <Boat
+                name="Fairline Targa 47"
+                description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit."
+                iconName="submarine"
+                picture={require('./img/fairline.jpg')}
+            />
+        </ScrollView>
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default AllBoats;
